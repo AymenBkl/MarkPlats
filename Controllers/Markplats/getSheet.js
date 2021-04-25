c8e8f194a8f9f72b27cf1008cb849b40f8c0930e
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-04-25 18:39:52 +0200

    building query working

--- Controllers/Markplats/getSheet.js
+++ Controllers/Markplats/getSheet.js
@@ -16,8 +16,9 @@ async function accessSpreedSheet() {
     console.log("info loaded");
     const sheetProduct = doc.sheetsByIndex[0];
     const sheetModels = doc.sheetsByIndex[4];
-    getUniqueModels(sheetModels);
-    //getProducts(sheetProduct);
+    const sheetUser = doc.sheetsByIndex[2];
+    let productModels = await getUniqueModels(sheetModels);
+    getProducts(sheetProduct,productModels,await getUser(sheetUser));
     /**await sheet.loadCells('A1:I50');
     const a1 =  sheet.getCell(0,1);
     for(let i=0;i<50;i++){
@@ -26,27 +27,54 @@ async function accessSpreedSheet() {
     }**/
 }
 
-async function getProducts(sheetProduct,rowsRubriekth) {
+async function getUser(sheetUser) {
+    const userRows = await sheetUser.getRows();
+    return userRows[0];
+
+}
+
+async function getProducts(sheetProduct,modelRows,user) {
     const rows = await sheetProduct.getRows();
     let keys = ['On / Off','Group','Rubric','Type','Storage','Condition Product','Maximum Price','Maximum Distance','Seller Active Since'];
     rows.map(row => {
+        constructQuery(row,modelRows,user);
         keys.map(key => {
-            console.log(key,row[key]);
         })
     })
     
 }   
 
 async function getUniqueModels(modelsRows){
-    let modelsSet = new Set();
+    let modelsMap = new Map();
     const rowCounts = (await modelsRows.getRows()).length;
     console.log(rowCounts,modelsRows.columnCount);
-    await modelsRows.loadCells(`B1:ZZ${rowCounts}`);
-    console.log(modelsRows.getCell(1,1).value)
+    await modelsRows.loadCells(`A1:ZZ${rowCounts}`);
     for(let i=1;i<rowCounts;i++){
-        for(let j=1;j<modelsRows.columnCount;j++)
-        modelsSet.add(await modelsRows.getCell(i,j).value);
+        let modelsSet = new Set();
+        for(let j=1;j<modelsRows.columnCount;j++){
+            modelsSet.add(await modelsRows.getCell(i,j).value);
+        }
+        modelsMap.set(modelsRows.getCell(i,0).value,modelsSet);
     }
-    console.log(modelsSet);
+    return modelsMap;
 }
 
+async function constructQuery(product,modelMap,user) {
+    let queryString = 'limit=100&offset=0';
+    console.log(user);
+    queryString += '&postcode=' + user.Postal; 
+    if (product.Rubric && product.Rubric != null){
+        queryString += await buildModelsQuery(modelMap.get(product.Rubric));
+    }
+    console.log(queryString)
+}
+
+async function buildModelsQuery(set) {
+    let stringModelQuery = '';
+    for(let modal of set){
+        if (modal && modal != null && modal != 'alle modellen'){
+            stringModelQuery += '&attributesByKey[]="' + modal +'"';
+        }
+    }
+    return stringModelQuery;
+}
